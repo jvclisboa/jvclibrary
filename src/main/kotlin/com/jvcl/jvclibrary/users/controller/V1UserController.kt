1f151d8355a04410b4359fb6d10c7fa847fc85e8
Victor Lisboa <jvclisboa@outlook.com>
2022-01-16 20:03:51 -0300

    changed imports

--- src/main/kotlin/com/jvcl/jvclibrary/users/controller/V1UserController.kt
+++ src/main/kotlin/com/jvcl/jvclibrary/users/controller/V1UserController.kt
@@ -4,7 +4,12 @@ import com.jvcl.jvclibrary.users.dto.V1CreateUserDTO
 import com.jvcl.jvclibrary.users.services.V1CreateUser
 import com.jvcl.jvclibrary.users.services.V1GetAllUsers
 import com.jvcl.jvclibrary.users.services.V1GetUserById
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/api/users")
