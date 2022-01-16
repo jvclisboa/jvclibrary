cd077fd756d41b3a2c432d6ba9170d37e22487c3
Victor Lisboa <jvclisboa@outlook.com>
2022-01-16 18:45:13 -0300

    changed val properties on users class to var

--- src/main/kotlin/com/jvcl/jvclibrary/users/model/V1User.kt
+++ src/main/kotlin/com/jvcl/jvclibrary/users/model/V1User.kt
@@ -8,10 +8,10 @@ import javax.persistence.*
 class V1User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id") val id: Long? = null,
-    @Column(name = "first_name") val firstName: String,
-    @Column(name = "last_name") val lastName: String,
-    @Column(name = "username") val username: String,
-    @Column(name = "password") val password: String,
-    @Column(name= "birth_date") val birthDate: LocalDate,
+    @Column(name = "user_id") var id: Long? = null,
+    @Column(name = "first_name") var firstName: String,
+    @Column(name = "last_name") var lastName: String,
+    @Column(name = "username") var username: String,
+    @Column(name = "password") var password: String,
+    @Column(name= "birth_date") var birthDate: LocalDate,
 )
